31e0cb6276ecd444a12b3d62e8979677ddacc9f0
Raf. Mavragani <efimavragani99@gmail.com>
2024-12-11 07:41:12 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Prints HelloWorld in standard output (console).
+ */
+   public class HelloWorld {
+       public static void main(String[] args) {
+         System.out.println("HelloWorld");
+    }
+}
+
+
